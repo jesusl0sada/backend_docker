516ff0b4185004e9816ad6d8451ee9aaaedb6944
Jesús Losada <jesus.losaca@gmail.com>
2025-03-05 16:11:15 +0100

    16:11
    updated

--- server.js
+++ server.js
@@ -58,6 +58,6 @@ app.get('/api/movies', async (req, res) => {
 });
 
 // Iniciar el servidor
-app.listen(PORT, () => {
-    console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`🚀 Servidor corriendo en http://0.0.0.0:${PORT}`);
 });
